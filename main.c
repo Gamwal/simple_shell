6392cbc94dce2d9a0015a6723b3d0d2e8cc794af
Gamaliel Adun <adungamaliel@gmail.com>
2022-11-14 11:25:39 +0000

    Backup

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int main(int argc, char **argv)
+{
+	char *prompt = "($) ";
+	char *buf = NULL;
+	size_t bufsize = 5;
+
+	(void)argc;
+	(void)argv;
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 5);
+		getline(&buf, &bufsize, stdin);
+
+		printf("%s", buf);
+	}
+	free(buf);
+	printf("buf is freed");
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+int takePrompt(int num);
+
+
+#endif /*SHELL_H*/
